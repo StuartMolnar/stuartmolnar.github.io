42a78c9d5d7eade2f88271a674b6cd155ea02bc3
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-16 14:53:03 -0800

    a

---
+++ script.js
@@ -0,0 +1,32 @@
+            
+// request({     
+//     url: 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016',     
+//     method: 'GET' 
+// }, function(error, response, body) {     
+//     var data = JSON.parse(body); 
+//     console.log(data);
+// });
+
+
+
+// let url = 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
+
+// fetch(url)
+//   .then(response => {
+//     return response.json()
+//    }).then(body => {
+
+//     var data = JSON.parse(body);
+//     console.log(body);
+//     console.log(data);
+//    }
+// );
+
+var request = require('request');    
+            
+request({     
+    url: 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016',     
+    method: 'GET' 
+}, function(error, response, body) {     
+    var data = JSON.parse(body); 
+});
