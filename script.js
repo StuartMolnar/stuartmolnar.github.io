535d17c227629f1687bb27775dceb90ee59ecaaa
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-16 14:53:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,24 @@
 
 
 
-// let url = 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
+let url = 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
 
-// fetch(url)
-//   .then(response => {
-//     return response.json()
-//    }).then(body => {
+fetch(url)
+  .then(response => {
+    return response.json()
+   }).then(body => {
 
-//     var data = JSON.parse(body);
-//     console.log(body);
-//     console.log(data);
-//    }
-// );
+    var data = JSON.parse(body);
+    console.log(body);
+    console.log(data);
+   }
+);
 
-var request = require('request');    
+// var request = require('request');    
             
-request({     
-    url: 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016',     
-    method: 'GET' 
-}, function(error, response, body) {     
-    var data = JSON.parse(body); 
-});
+// request({     
+//     url: 'https://data.usajobs.gov/api/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016',     
+//     method: 'GET' 
+// }, function(error, response, body) {     
+//     var data = JSON.parse(body); 
+// });
