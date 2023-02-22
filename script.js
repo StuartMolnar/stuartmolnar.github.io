07f7c52a0490c71765216f2177b472fc2947cc49
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-21 18:57:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,12 @@
 
 let url = 'https://usajobs-cors-proxy.westus3.cloudapp.azure.com:443/proxy/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
 
-console.log('accessing data...');
-
 fetch(url)
   .then(response => {
     return response.json()
    }).then(body => {
 
+    var data = JSON.parse(body);
     console.log(body);
     console.log(data);
    }
