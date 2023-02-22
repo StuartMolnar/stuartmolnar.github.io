805fbd53bb41791ecf67a6fa4a0c7476987210a4
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-21 18:58:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 // });
 
 
+console.log('accessing data...');
 
 let url = 'https://usajobs-cors-proxy.westus3.cloudapp.azure.com:443/proxy/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
 
