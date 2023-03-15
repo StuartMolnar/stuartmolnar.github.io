d882641edcd5f8db8018cf7aa4a3366b9f4e1b20
StuartMolnar <stuartmolnar@hotmail.com>
2023-03-14 20:01:56 -0700

    carousel test

---
+++ js/index-carousel.js
@@ -0,0 +1,351 @@
+const carouselImages = ['images/Unsorted/geochem-11.jpeg',
+                        'images/Unsorted/crew-8.jpeg',
+                        'images/Unsorted/P2120014.jpeg',
+                        'images/Unsorted/frontpage-1-4.jpeg',
+                        'images/Unsorted/donkeys.jpeg',
+                        'images/Unsorted/Scenic001.jpeg',
+                        'images/Unsorted/1_IMG_4029.jpeg',
+                        'images/Unsorted/IMG_4136.jpeg',
+                        'images/Unsorted/p7180580-1.jpeg',
+                        'images/Unsorted/p7180584.jpeg',
+                        'images/trenching/1.jpeg',
+                        'images/trenching/19.jpeg',
+                        'images/camp_building/35.jpeg',
+                        'images/project_management/3.jpeg',
+                        'images/project_management/2.jpeg',
+                        'images/geophysics/1.jpeg',
+                        'images/line_cutting/1.jpeg',
+                        'images/geophysics/8.jpeg',
+                        'images/geochemistry/1.jpeg',
+                        'images/geochemistry/2.jpeg',
+                        'images/geochemistry/32.jpeg',
+                        'images/project_management/4.jpeg',
+                        'images/geochemistry/15.jpeg',
+                        'images/geochemistry/8.jpeg',
+                        'images/trenching/19.jpeg',
+                        'images/project_management/9.jpeg',
+                        'images/geochemistry/13.jpeg',
+                        'images/geochemistry/46.jpeg',
+                        'images/geochemistry/47.jpeg',
+                        'images/geochemistry/50.jpeg',
+                        'images/drill_programs/2.jpeg',
+                        'images/geology/59.jpeg',
+                        'images/geochemistry/13.jpeg',
+                        'images/drill_programs/9.jpeg',
+                        'images/geology/3.jpeg',];
+                        
+
+
+const carousel = document.querySelector('.carousel-images');
+function loadImages(){
+
+  for (let i = 0; i < carouselImages.length; i++) {
+    const imageSrc = carouselImages[i];
+    console.log(imageSrc);
+
+    const div = document.createElement('div');
+    if (window.innerWidth <= 640 && i > 0) {
+      console.log("hidden mobile");
+      div.className = "hidden carousel-cell animate__animated h-[500px]";
+    } else if (i > 2) {
+      console.log("hidden default");
+      div.className = "hidden carousel-cell animate__animated h-[500px]";
+    } else {
+      console.log('not hidden');
+      div.className = "carousel-cell animate__animated h-[500px]";
+    }
+    const img = document.createElement('img');
+    img.className = "object-cover w-full";
+    img.src = imageSrc;
+    img.onclick = showLightbox;
+    div.appendChild(img);
+    carousel.appendChild(div);
+  }
+  
+}
+
+window.addEventListener('load', loadImages);
+
+function getVisibleImagesCount() {
+    return window.innerWidth <= 640 ? 1 : 3;
+}
+
+
+/* --- update carousel on resize --- */
+// function onResize() {
+//     const carouselCells = document.querySelectorAll('#carousel-images .carousel-cell');
+//     const visibleImagesCount = getVisibleImagesCount();
+  
+//     for (let i = 0; i < carouselCells.length; i++) {
+//       if (i < visibleImagesCount) {
+//         carouselCells[i].classList.remove('hidden');
+//       } else {
+//         carouselCells[i].classList.add('hidden');
+//       }
+//     }
+//   }
+  
+  
+//   window.addEventListener('resize', onResize);
+
+
+/* ----- carousel navigation ----- */
+  
+function prevImage() {
+    const carouselCells = document.querySelectorAll('#carousel-images .carousel-cell');
+    let firstVisibleIndex = -1;
+    const visibleImagesCount = getVisibleImagesCount();
+
+    for (let i = 0; i < carouselCells.length; i++) {
+        if (!carouselCells[i].classList.contains('hidden')) {
+            firstVisibleIndex = i;
+            break;
+        }
+    }
+
+    if (firstVisibleIndex > 0) {
+        carouselCells[firstVisibleIndex - 1].classList.remove('hidden');
+        setTimeout(() => {
+          carouselCells[firstVisibleIndex + visibleImagesCount - 1].classList.add('hidden');
+        }, 25);
+      }
+}
+  
+function nextImage() {
+    const carouselCells = document.querySelectorAll('#carousel-images .carousel-cell');
+    let lastVisibleIndex = -1;
+    const visibleImagesCount = getVisibleImagesCount();
+
+    for (let i = carouselCells.length - 1; i >= 0; i--) {
+        if (!carouselCells[i].classList.contains('hidden')) {
+        lastVisibleIndex = i;
+        break;
+        }
+    }
+
+    if (lastVisibleIndex < carouselCells.length - 1) {
+        carouselCells[lastVisibleIndex + 1].classList.remove('hidden');
+        setTimeout(() => {
+          carouselCells[lastVisibleIndex - visibleImagesCount + 1].classList.add('hidden');
+        }, 25);
+      }
+}
+  
+
+/* ----- carousel tap ----- */
+
+if (window.innerWidth <= 640) {
+  const hammer = new Hammer(carousel);
+
+  hammer.on('tap', function(event) {
+    const carouselWidth = carousel.offsetWidth;
+    const tapX = event.center.x;
+  
+    if (tapX < carouselWidth * 0.3) {
+      prevImage();
+    } else if (tapX > carouselWidth * 0.7) {
+      nextImage();
+    }
+  });
+}
+
+
+/* ----- lightbox ----- */
+// Get the lightbox container and image elements
+const lightboxContainer = document.getElementById('lightbox');
+const lightboxImage = lightboxContainer.querySelector('img');
+const lightboxNavButtonPrev = lightboxContainer.querySelector('.lightbox-nav-button-prev');
+const lightboxNavButtonNext = lightboxContainer.querySelector('.lightbox-nav-button-next');
+const lightboxCloseButton = lightboxContainer.querySelector('.lightbox-close-button');
+const lightboxCount = document.getElementById('lightbox-count');
+
+  // Set the position of the lightbox count element
+  const setPosition = () => {
+    const imgWidth = lightboxImage.offsetWidth;
+    const imgHeight = lightboxImage.offsetHeight;
+    lightboxCount.style.left = `calc(50% - ${imgWidth / 2}px)`;
+    lightboxCount.style.top = `calc(50% - ${imgHeight / 2}px - 40px)`;
+  }
+  window.requestAnimationFrame(setPosition);
+  window.addEventListener('resize', setPosition);
+
+function showLightbox(event) {  
+  // Get a reference to the clicked image element
+  const clickedImage = event.target;
+
+  // Set the lightbox image source to the clicked image source
+  lightboxImage.src = clickedImage.src;
+  // Show the lightbox container
+  lightboxContainer.style.display = 'block';
+
+  // Disable scrolling on the document body
+  document.body.style.overflow = 'hidden';
+
+  // Get all images in the grid and the index of the clicked image
+  const images = document.querySelectorAll('#carousel-images .carousel-cell img');
+  const currentIndex = Array.prototype.indexOf.call(images, clickedImage);
+
+  // Display the current image number
+  const countElement = document.getElementById('lightbox-count');
+  countElement.textContent = '(' + (currentIndex + 1) + '/' + images.length + ')';
+
+  // Add click event listener to the lightbox container to hide it when clicked
+  lightboxContainer.addEventListener('click', function(event) {
+    if (event.target === this) {
+      hideLightbox();
+    }
+  });
+
+  // Play the zoomIn animation
+  lightboxImage.classList.add('animate__animated', 'animate__zoomIn');
+
+  // Remove the zoomIn animation after it's played
+  lightboxImage.addEventListener('animationend', function() {
+    lightboxImage.classList.remove('animate__animated', 'animate__zoomIn');
+  }, { once: true });
+
+  // Set the position of the lightbox count element
+  window.requestAnimationFrame(() => {
+    const imgWidth = lightboxImage.offsetWidth;
+    const imgHeight = lightboxImage.offsetHeight;
+    lightboxCount.style.left = `calc(50% - ${imgWidth / 2}px)`;
+    lightboxCount.style.top = `calc(50% - ${imgHeight / 2}px - 40px)`;
+  });
+}
+
+
+
+
+function fadeIn(element, direction, countElement) {
+  const animationClass = direction === 'left' ? 'animate__fadeInLeftBig' : 'animate__fadeInRightBig';
+  element.classList.remove('animate__fadeInLeftBig', 'animate__fadeInRightBig', 'animate__fadeOutLeftBig', 'animate__fadeOutRightBig');
+  element.classList.add('animate__animated', animationClass);
+  element.style.display = 'block';
+  if (countElement) {
+    countElement.classList.remove('animate__fadeOutLeftBig', 'animate__fadeOutRightBig', 'animate__fadeInLeftBig', 'animate__fadeInRightBig');
+    countElement.classList.add('animate__animated', animationClass);
+    countElement.style.display = 'block';
+  }
+}
+
+
+function hideLightbox() {
+  lightboxContainer.style.display = 'none';
+
+  // Reset the lightbox image source
+  lightboxImage.src = '';
+
+  // Remove all animation classes
+  lightboxImage.classList.remove('animate__animated', 'animate__zoomOut', 'animate__fadeInLeftBig', 'animate__fadeInRightBig', 'animate__fadeOutLeftBig', 'animate__fadeOutRightBig');
+
+  // Enable scrolling on the document body
+  document.body.style.overflow = '';
+}
+
+lightboxCloseButton.addEventListener('click', function() {
+  hideLightbox();
+});
+
+lightboxNavButtonPrev.addEventListener('click', function() {
+    prevImage();
+  // Get all images in the grid
+  const images = document.querySelectorAll('#carousel-images .carousel-cell img');
+
+  // Get the current image index
+  let currentIndex = -1;
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].src === lightboxImage.src) {
+      currentIndex = i;
+      break;
+    }
+  }
+
+  // Set the lightbox image source to the previous image source
+  if (currentIndex > 0) {
+    const newImageSrc = images[currentIndex - 1].src;
+    const direction = 'left';
+
+    // Update the current image number
+    const countElement = document.getElementById('lightbox-count');
+    countElement.textContent = '(' + (currentIndex) + '/' + images.length + ')';
+    
+
+    fadeOut(lightboxImage, direction, function() {
+      lightboxImage.src = newImageSrc;
+      
+      // Set the position of the lightbox count element
+      window.requestAnimationFrame(() => {
+        const imgWidth = lightboxImage.offsetWidth;
+        const imgHeight = lightboxImage.offsetHeight;
+        lightboxCount.style.left = `calc(50% - ${imgWidth / 2}px)`;
+        lightboxCount.style.top = `calc(50% - ${imgHeight / 2}px - 40px)`;
+      });
+
+      fadeIn(lightboxImage, direction, countElement);
+    }, countElement);
+  }
+});
+
+
+lightboxNavButtonNext.addEventListener('click', function() {
+    nextImage();
+  // Get all images in the grid
+  const images = document.querySelectorAll('#carousel-images .carousel-cell img');
+
+  // Get the current image index
+  let currentIndex = -1;
+  for (let i = 0; i < images.length; i++) {
+    if (images[i].src === lightboxImage.src) {
+      currentIndex = i;
+      break;
+    }
+  }
+  // Set the lightbox image source to the next image source
+  if (currentIndex < images.length - 1) {
+    const newImageSrc = images[currentIndex + 1].src;
+    const direction = 'right';
+    // Declare countElement variable
+    const countElement = document.getElementById('lightbox-count');
+    fadeOut(lightboxImage, direction, function() {
+      // Update the current image number
+      countElement.textContent = '(' + (currentIndex + 2) + '/' + images.length + ')';
+
+      // Set the lightbox image source to the new image source
+      lightboxImage.src = newImageSrc;
+
+      // Set the position of the lightbox count element
+      window.requestAnimationFrame(() => {
+        const imgWidth = lightboxImage.offsetWidth;
+        const imgHeight = lightboxImage.offsetHeight;
+        lightboxCount.style.left = `calc(50% - ${imgWidth / 2}px)`;
+        lightboxCount.style.top = `calc(50% - ${imgHeight / 2}px - 40px)`;
+      });
+
+      fadeIn(lightboxImage, direction, countElement); // <-- pass countElement as the third parameter
+    }, countElement);
+  }
+});
+
+
+
+
+
+function fadeOut(element, direction, callback, countElement) {
+  const animationClass = direction === 'left' ? 'animate__fadeOutRightBig' : 'animate__fadeOutLeftBig';
+  element.classList.add('animate__animated', animationClass);
+  const onAnimationEnd = function() {
+    element.removeEventListener('animationend', onAnimationEnd);
+    element.style.display = 'none';
+    element.classList.remove('animate__animated', animationClass, 'animate__fadeInLeftBig', 'animate__fadeInRightBig');
+    if (countElement) {
+      countElement.style.display = 'none';
+      countElement.classList.remove('animate__animated', animationClass, 'animate__fadeInLeftBig', 'animate__fadeInRightBig');
+    }
+    callback();
+  };
+  element.addEventListener('animationend', onAnimationEnd);
+  if (countElement) {
+    countElement.classList.add('animate__animated', animationClass);
+  }
+}
+
--- js/index.js
+++ js/index.js
@@ -1,72 +1,3 @@
-const carouselImages = ['images/Unsorted/geochem-11.jpeg',
-                        'images/Unsorted/crew-8.jpeg',
-                        'images/Unsorted/P2120014.jpeg',
-                        'images/Unsorted/frontpage-1-4.jpeg',
-                        'images/Unsorted/donkeys.jpeg',
-                        'images/Unsorted/Scenic001.jpeg',
-                        'images/Unsorted/1_IMG_4029.jpeg',
-                        'images/Unsorted/IMG_4136.jpeg',
-                        'images/Unsorted/p7180580-1.jpeg',
-                        'images/Unsorted/p7180584.jpeg',
-                        'images/trenching/1.jpeg',
-                        'images/trenching/19.jpeg',
-                        'images/camp_building/35.jpeg',
-                        'images/project_management/3.jpeg',
-                        'images/project_management/2.jpeg',
-                        'images/geophysics/1.jpeg',
-                        'images/line_cutting/1.jpeg',
-                        'images/geophysics/8.jpeg',
-                        'images/geochemistry/1.jpeg',
-                        'images/geochemistry/2.jpeg',
-                        'images/geochemistry/32.jpeg',
-                        'images/project_management/4.jpeg',
-                        'images/geochemistry/15.jpeg',
-                        'images/geochemistry/8.jpeg',
-                        'images/trenching/19.jpeg',
-                        'images/project_management/9.jpeg',
-                        'images/geochemistry/13.jpeg',
-                        'images/geochemistry/46.jpeg',
-                        'images/geochemistry/47.jpeg',
-                        'images/geochemistry/50.jpeg',
-                        'images/drill_programs/2.jpeg',
-                        'images/geology/59.jpeg',
-                        'images/geochemistry/13.jpeg',
-                        'images/drill_programs/9.jpeg',
-                        'images/geology/3.jpeg',];
-
-const carousel = document.querySelector('.carousel-images');
-const carouselCells = document.querySelectorAll('.carousel-cell img');
-let currentImageIndex = 0;
-
-function prevImage() {
-  currentImageIndex = (currentImageIndex === 0) ? carouselImages.length - 1 : currentImageIndex - 1;
-  for (let i = 0; i < carouselCells.length; i++) {
-    carouselCells[i].src = carouselImages[(currentImageIndex + i) % carouselImages.length];
-  }
-}
-
-function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
-  for (let i = 0; i < carouselCells.length; i++) {
-    carouselCells[i].src = carouselImages[(currentImageIndex + i) % carouselImages.length];
-  }
-}
-
-if (window.innerWidth <= 640) {
-  const hammer = new Hammer(carousel);
-
-  hammer.on('tap', function(event) {
-    const carouselWidth = carousel.offsetWidth;
-    const tapX = event.center.x;
-  
-    if (tapX < carouselWidth * 0.3) {
-      prevImage();
-    } else if (tapX > carouselWidth * 0.7) {
-      nextImage();
-    }
-  });
-}
-
 function setHeroAndVideoHeight() {
   // Get the height of navbar-max
   const navbarMax = document.getElementById('navbar-max');
@@ -103,7 +34,7 @@ function handleDivIntersect(entries) {
 }
 
 const divObserver = new IntersectionObserver(handleDivIntersect, {
-  threshold: 0.0, // trigger when element is 0% in viewport
+  threshold: 0.2, // trigger when element is 20% in viewport
 });
 
 for (let i = 0; i < indexChild1.length; i++) {
@@ -113,80 +44,5 @@ for (let i = 0; i < indexChild1.length; i++) {
 for (let i = 0; i < indexChild2.length; i++) {
   divObserver.observe(indexChild2[i]);
 }
-
-
-
-// Get the lightbox container and image elements
-const lightboxContainer = document.getElementById('lightbox');
-const lightboxImage = lightboxContainer.querySelector('img');
-
-function showLightbox(event) {
-  // Get a reference to the clicked image element
-  const clickedImage = event.target;
-
-  // Check if the screen resolution is less than or equal to 640 pixels
-  if (window.innerWidth <= 640) {
-    // Calculate the 30% and 70% positions of the image
-    const imgWidth = clickedImage.width;
-    const thirtyPercent = imgWidth * 0.3;
-    const seventyPercent = imgWidth * 0.7;
-
-    // Check if the click occurred within the 30%-70% range of the image width
-    const clickX = event.clientX - clickedImage.offsetLeft;
-    if (clickX >= thirtyPercent && clickX <= seventyPercent) {
-      // Set the lightbox image source to the clicked image source
-      lightboxImage.src = clickedImage.src;
-
-      // Show the lightbox container
-      lightboxContainer.style.display = 'block';
-
-      // Disable scrolling on the document body
-      document.body.style.overflow = 'hidden';
-    }
-  } else {
-    // Set the lightbox image source to the clicked image source
-    lightboxImage.src = clickedImage.src;
-
-    // Show the lightbox container
-    lightboxContainer.style.display = 'block';
-
-    // Disable scrolling on the document body
-    document.body.style.overflow = 'hidden';
-  }
-}
-
-
-function hideLightbox() {
-  // Hide the lightbox container
-  lightboxContainer.style.display = 'none';
-
-  // Reset the lightbox image source
-  lightboxImage.src = '';
-
-  // Enable scrolling on the document body
-  document.body.style.overflow = 'auto';
-}
-
-// Add click event listeners to the lightbox container and close button to hide the lightbox
-lightboxContainer.addEventListener('click', function(event) {
-  // Only hide the lightbox if the click occurred on the background
-  if (event.target === this) {
-    hideLightbox();
-  }
-});
-
-const closeButton = document.createElement('button');
-closeButton.innerHTML = 'Close';
-closeButton.addEventListener('click', function() {
-  hideLightbox();
-});
-lightboxContainer.appendChild(closeButton);
-
-// Add keydown event listener to the document to close the lightbox on escape key press
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'Escape' && lightboxContainer.style.display === 'block') {
-    hideLightbox();
-  }
-});
 
 
