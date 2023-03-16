fa6bca95932f5183da20e59f2cf4df993cdf4aea
StuartMolnar <stuartmolnar@hotmail.com>
2023-03-15 23:46:24 -0700

    updates

--- js/navbar.js
+++ js/navbar.js
@@ -1,54 +1,55 @@
 const initMobileMenu = () => {
 
     
-  hamburgerBtn = document.getElementById('hamburger-button');
-  const mobileMenu = document.getElementById('mobile-menu');
+    hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
 
-  const toggleMenu = () => {
-      mobileMenu.classList.toggle('hidden');
-      mobileMenu.classList.toggle('flex');
-      document.body.classList.toggle('overflow-y-hidden')
-  }
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        document.body.classList.toggle('overflow-y-hidden')
+    }
 
-  hamburgerBtn.addEventListener('click', toggleMenu);
-  mobileMenu.addEventListener('click', toggleMenu)
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu)
 
 }
 
-
+  
 const initNavbarMinimizer = () => {
-const header = document.getElementById('navbar-full');
-const backToTop = document.getElementById('back-to-top-button');
+  const header = document.getElementById('navbar-full');
+  const backToTop = document.getElementById('back-to-top-button');
 
-let lastScrollTop = 0;
+  let lastScrollTop = 0;
 
-window.addEventListener('scroll', () => {
-  const scrollTop = window.pageYOffset;
-  const scrollThreshold = window.innerHeight * 0.4;
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset;
+    const scrollThreshold = window.innerHeight * 0.4;
 
-  if (scrollTop > scrollThreshold) {
-    if (scrollTop > lastScrollTop) {
-      // Scrolling down
-      header.classList.add('translate-y-up');
-      backToTop.classList.remove('hidden');
-      backToTop.classList.add('flex');
-    } else {
-      // Scrolling up
-      header.classList.remove('translate-y-up');
-      backToTop.classList.add('hidden');
-      backToTop.classList.remove('flex');
+    if (scrollTop > scrollThreshold) {
+      if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        header.classList.add('translate-y-up');
+        backToTop.classList.remove('hidden');
+        backToTop.classList.add('flex');
+      } else {
+        // Scrolling up
+        header.classList.remove('translate-y-up');
+        backToTop.classList.add('hidden');
+        backToTop.classList.remove('flex');
+      }
     }
-  }
 
-  lastScrollTop = scrollTop;
-});
+    lastScrollTop = scrollTop;
+  });
 };
 
 
 
 const initNavbarSpacer = () => {
-const navbarFull = document.getElementById('navbar-full');
-const navbarPlaceholder = document.getElementById('navbar-spacer');
-navbarPlaceholder.style.height = `${navbarFull.offsetHeight}px`;
+  const navbarFull = document.getElementById('navbar-full');
+  const navbarPlaceholder = document.getElementById('navbar-spacer');
+  navbarPlaceholder.style.height = `${navbarFull.offsetHeight}px`;
 }
 
+  
